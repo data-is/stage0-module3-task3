56f0323dd7405c4ae2ec97b63a1f8bd04dc2ab0e
Luka <code.coding2001@gmail.com>
2023-10-07 13:48:35 +0400

    come on !

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -7,22 +7,14 @@ public class ReassigningValues {
         int second = 10;
         int third = 100;
 
-        // Print the initial values of the variables
-        System.out.println("first: " + first);
-        System.out.println("second: " + second);
-        System.out.println("third: " + third);
-
-        // Initialize three variables to the initial values of the first three variables
         int linkToFirst = first;
         int linkToSecond = second;
         int linkToThird = third;
 
-        // Reassign the first three variables
         first = 15;
         second = 6;
         third = 4;
 
-        // Print the values of all the variables
         System.out.println("first: " + first);
         System.out.println("second: " + second);
         System.out.println("third: " + third);
